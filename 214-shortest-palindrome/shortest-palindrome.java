9c8cca56d8a7e8d8053d448fde3839fa4c7b5223
ashish-id16 <154454215+ashish-id16@users.noreply.github.com>
2026-06-19 06:59:22 +0530

    Time: 9 ms (34.10%) | Memory: 47 MB (32.76%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        if (s == null || s.length() <= 1) return s;
+        
+        String reversed = new StringBuilder(s).reverse().toString();
+        String combined = s + "#" + reversed;
+        
+        int[] lps = new int[combined.length()];
+        for (int i = 1; i < combined.length(); i++) {
+            int j = lps[i - 1];
+            while (j > 0 && combined.charAt(i) != combined.charAt(j)) {
+                j = lps[j - 1];
+            }
+            if (combined.charAt(i) == combined.charAt(j)) {
+                j++;
+            }
+            lps[i] = j;
+        }
+        
+        int longestPalindromePrefixLength = lps[combined.length() - 1];
+        String suffixToAdd = reversed.substring(0, s.length() - longestPalindromePrefixLength);
+        
+        return suffixToAdd + s;
+    }
+}
